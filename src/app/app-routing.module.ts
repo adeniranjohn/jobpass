5e850529569b3e5acbfa751881e757b7ba16e343
John Adeniran <jasolajohn@gmail.com>
2020-05-28 22:52:30 +0100

    resume profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,9 @@ const routes: Routes = [
                     { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)},
                     { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
                     { path: 'quiz', loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule) },
-                    { path: 'jobs', loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule) }
+                    { path: 'jobs', loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule) },
+                    { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) },
+                    { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }
 ];
 
 @NgModule({
